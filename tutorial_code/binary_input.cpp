ff074256af011fbd27f0a8e71132cb328cd61c3e
faridulhaque <faridulhaquemurshed@gmail.com>
2023-07-09 22:57:39 +0600

    done

---
+++ tutorial_code/binary_input.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+
+        cout << f->value << " ";
+
+        if (f->left != NULL)
+        {
+            q.push(f->left);
+        }
+        if (f->right != NULL)
+        {
+            q.push(f->right);
+        }
+    }
+}
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+    if (root != NULL)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+        int l, r;
+        cin >> l >> r;
+
+        Node *left;
+        Node *right;
+
+        if (l == -1)
+            left = NULL;
+        else
+            left = new Node(l);
+
+        if (r == -1)
+            right = NULL;
+        else
+            right = new Node(r);
+
+        p->left = left;
+        p->right = right;
+        
+        if (left != NULL)
+            q.push(left);
+
+        if (right != NULL)
+            q.push(right);
+    }
+
+    return root;
+}
+
+int main()
+{
+
+    Node *root = input_tree();
+
+    level_order(root);
+
+    return 0;
+}
